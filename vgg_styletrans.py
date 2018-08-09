c64e190b622da70853f7253affca3554e31c5b4a
Vincent Marron <VinceMarron@users.noreply.github.com>
2018-08-09 17:19:19 -0500

    bfgs intermediate frame output and small changes

--- vgg_styletrans.py
+++ vgg_styletrans.py
@@ -1,10 +1,9 @@
-# Copyright (c) 2017 Vincent Marron | Released under MIT License
+# Copyright (c) 2018 Vincent Marron | Released under MIT License
 
 import tensorflow as tf
 import numpy as np
 from PIL import Image
 import time
-
 
 class TransferStyle(object):
   """Modifies a 'subject_image' to exhibit the visual style of a 'style_image'
@@ -72,11 +71,11 @@ class TransferStyle(object):
       
       for layer in self.all_layers[:self.all_layers.index(last_layer)+1]:
           
-        if layer[:2] == 're': x = tf.nn.relu(x)
+        if layer[:4] == 'relu': x = tf.nn.relu(x)
 
-        elif layer[:2] == 'po': x = self.pool_func(x, pool_type)
+        elif layer[:4] == 'pool': x = pool_func(x, pool_type)
 
-        elif layer[:2] == 'co':
+        elif layer[:4] == 'conv':
           kernel = self.vgg_ph[layer+'_kernel']
           bias = self.vgg_ph[layer+'_bias']
 
@@ -84,29 +83,20 @@ class TransferStyle(object):
                                           strides=(1, 1, 1, 1),
                                           padding='SAME'),bias)
 
-          layer_shape = tf.shape(x, out_type=tf.int32)
-
-          #flattens image tensor to (#pixels x #channels) assumes batch=1
-          #treats each pixel as an observation of Gaussian random vector
-          #in R^(#channels) and infers parameters
-          stl_activs = tf.reshape(x, [layer_shape[1]*layer_shape[2], layer_shape[3]])
-          mean_stl_activs = tf.reduce_mean(stl_activs, axis=0, keep_dims=True)
-          covar_stl_activs = (tf.matmul(stl_activs - mean_stl_activs,
-                                        stl_activs - mean_stl_activs, transpose_a=True)/
-                              tf.cast(layer_shape[1]*layer_shape[2], tf.float32))
+          mean, cov = calc_2_moments(x)
 
           #takes root of covar_stl_activs
-          #(necessary for wdist, as tf cannot take eig of non-symmetric matrices)
-          eigvals,eigvects = tf.self_adjoint_eig(covar_stl_activs)
+          #(necessary for later step, as tf cannot take eig of non-symmetric matrices)
+          eigvals,eigvects = tf.self_adjoint_eig(cov)
           eigroot_mat = tf.diag(tf.sqrt(tf.maximum(eigvals,0.)))
-          root_covar_stl_activs = tf.matmul(tf.matmul(eigvects, eigroot_mat)
+          root_cov = tf.matmul(tf.matmul(eigvects, eigroot_mat)
                                                 ,eigvects,transpose_b=True)
 
-          trace_covar_stl = tf.reduce_sum(tf.maximum(eigvals,0))
+          tr_cov = tf.reduce_sum(tf.maximum(eigvals,0))
           
-          self.style_desc[layer] = (mean_stl_activs,
-                                    trace_covar_stl,
-                                    root_covar_stl_activs)
+          self.style_desc[layer] = (mean,
+                                  tr_cov,
+                                  root_cov)
       if eval_out==True:
         with tf.Session(graph=self.graph, config=self.config) as sess:
           self.style_desc = sess.run(self.style_desc, feed_dict=self.feed_dict)
@@ -125,6 +115,7 @@ class TransferStyle(object):
     """
     with self.graph.as_default():
       self.loss =0
+      
       self.synth_arr = tf.Variable((np.expand_dims(subj_image,0)[:,:,:,:3]).astype('float32')
                                    , trainable=True, dtype=tf.float32)
 
@@ -133,11 +124,11 @@ class TransferStyle(object):
       y = self.synth_arr-self.mean_pixel
 
       for layer in self.all_layers:
-        if layer[:2] == 're': y = tf.nn.relu(y)
+        if layer[:4] == 'relu': y = tf.nn.relu(y)
 
-        elif layer[:2] == 'po': y= self.pool_func(y, pool_type)
+        elif layer[:4] == 'pool': y= pool_func(y, pool_type)
 
-        elif layer[:2] == 'co':
+        elif layer[:4] == 'conv':
           if layer not in self.style_desc.keys(): break
           
           kernel = self.vgg_ph[layer+'_kernel']
@@ -147,51 +138,24 @@ class TransferStyle(object):
                                           strides=(1, 1, 1, 1),
                                       padding='SAME'),bias)
           if layer in loss_layers:
-            layer_shape = tf.shape(y, out_type=tf.int32)
-
-            #flattens image to (#pixels x #channels) assumes batch=1
-            #treats each pixel as an observation of Gaussian random vector
-            #in R^(#channels) and infers parameters
-
-            synth_activs = tf.reshape(y, [layer_shape[1]*layer_shape[2], layer_shape[3]])
-            mean_synth_activs = tf.reduce_mean(synth_activs, axis=0, keep_dims=True)
-            covar_synth_activs = (tf.matmul(synth_activs - mean_synth_activs,
-                              synth_activs - mean_synth_activs, transpose_a=True)/
-                              tf.cast(layer_shape[1]*layer_shape[2], tf.float32))
+            mean_synth, cov_synth = calc_2_moments(y)
             
-            trace_covar_synth = tf.trace(covar_synth_activs)
+            dist = calc_l2wass_dist(self.style_desc[layer], mean_synth, cov_synth)
             
-            mean_stl_activs, trace_covar_stl, root_covar_stl_activs = self.style_desc[layer]
+            self.loss += dist
             
-            mean_diff_squared = tf.reduce_sum(tf.square(mean_stl_activs-mean_synth_activs))
 
-            covar_prod = tf.matmul(tf.matmul(root_covar_stl_activs,covar_synth_activs),root_covar_stl_activs)
-            #trace of sqrt of matrix is sum of sqrts of eigenvalues
-            var_overlap = tf.reduce_sum(tf.sqrt(tf.maximum(
-                          tf.self_adjoint_eig(covar_prod)[0],0.)))
-
-            #loss can be slightly negative because of the 'maximum' on eigvals of covar_prod
-            #could fix with trace_covar_synth = tf.reduce_sum(tf.maximum(eigvals_synth,0))
-            #but that would mean extra non-critical computation
-
-            self.loss += mean_diff_squared+trace_covar_stl+trace_covar_synth-2*var_overlap
-
-
-
-  def synthesize_image(self, savename, optimizer = 'adam', steps=50, lr=1., log_ims=True, report_int=10,
-                      blur_int=0, blur_dim=15, blur_sig_sq=1.):
+  def synthesize_image(self, savename, optimizer = 'adam', steps=50, lr=1., 
+                       log_ims=True, report_int=10,):
     """invokes an optimizer and creates synthesized image
 
     'savename' (str): ending in PIL image format (.jpg, .png, etc.)
-    'optimizer' (str): 'adam', 'rmsprop' or 'bfgs' ('bfgs' runs through scipy interface,
-      cannot log/blur images or report status for this option & 'lr' is ignored)
+    'optimizer' (str): 'adam', 'rmsprop' or 'bfgs' ('bfgs' runs through scipy,
+       'lr' and 'report_int' are ignored)
     'steps': (int>0) number of iterations
     'lr': (float) the learning rate for adam/rmsprop
-    'log_ims' (boolean): if 'True' stores a snapshot of image being created at each step
+    'log_ims' (boolean): if 'True' snapshots image being created at each step
     'report_int' (int>0): interval of steps at which time and loss will be printed
-    'blur_int' (int, ignored if 0): interval at which to apply Gaussian blur to synth_image.
-    'blur_dim' (int>0, preferably odd): pixel width & height of Gaussian kernel
-    'blur_sig_sq' (float): sigma squared for Gaussian/RBF kernel
 
     """
 
@@ -202,13 +166,25 @@ class TransferStyle(object):
 
       if optimizer == 'bfgs':
         sess.run(tf.global_variables_initializer())
+        
         optimizer = tf.contrib.opt.ScipyOptimizerInterface(self.loss,
-                                                         options={'maxiter': steps},
-                                                         var_list = [self.synth_arr],
-                                                         var_to_bounds={self.synth_arr: (0.,255.)})
-
+                                   options={'maxiter': steps},
+                                   var_list = [self.synth_arr],
+                                   var_to_bounds={self.synth_arr: (0.,255.)})
+        
         print("loss:","{:,.2f}".format(sess.run(self.loss, feed_dict=self.feed_dict)))
-        optimizer.minimize(session=sess, feed_dict=self.feed_dict)
+        
+        if log_ims:
+          def logimage(arr):
+            self.imagelist.append(np.clip(arr[0], 0, 255).astype('uint8'))
+        
+          optimizer.minimize(session=sess, fetches=[self.synth_arr], 
+                           feed_dict=self.feed_dict, loss_callback=logimage)
+        
+        else:
+          optimizer.minimize(session=sess, feed_dict=self.feed_dict)
+          
+        
 
       else:
         if optimizer == 'rmsprop':
@@ -224,12 +200,7 @@ class TransferStyle(object):
         print("loss:","{:,.2f}".format(sess.run(self.loss, feed_dict=self.feed_dict)))
 
         for step in range(steps):
-          
-          if blur_int>0 and (step+1)%blur_int==0:
-            sess.run(tf.assign(self.synth_arr, 
-                               self.blur_func(self.synth_arr, blur_dim, blur_sig_sq)
-                               , use_locking=True))
-          
+                   
           sess.run(self.train_op, feed_dict=self.feed_dict)
 
           if log_ims:
@@ -239,7 +210,7 @@ class TransferStyle(object):
             print("step:", step+1,
                 "time:", "{:4.1f}".format(time.time()-start),
                 "loss:", "{:,.2f}".format(sess.run(self.loss, feed_dict=self.feed_dict)))
-
+            
       sess.run(tf.assign(self.synth_arr, 
                          tf.clip_by_value(self.synth_arr, 0, 255), 
                          use_locking=True))
@@ -252,41 +223,71 @@ class TransferStyle(object):
 
       print("synthesized image saved:", savename)
 
-  @staticmethod    
-  def pool_func(x, pool_type):
-    """ runs 2x2 pooling function on spatial dimensions of x
+  
+def pool_func(x, pool_type):
+  """Runs 2x2 pooling function on spatial dimensions of x
 
-    Args:
-        x (tensor): 4-d (batch x height x width x channels)
-        pool_type (str): 'avg', 'max', or 'none'
+  Args:
+      x (tensor): 4-d (batch x height x width x channels)
+      pool_type (str): 'avg', 'max', or 'none'
 
-    """
-    if pool_type=='avg':
-      out = tf.nn.avg_pool(x, (1, 2, 2, 1), (1, 2, 2, 1), padding='SAME')
-    elif pool_type=='max':
-      out = tf.nn.max_pool(x, (1, 2, 2, 1), (1, 2, 2, 1), padding='SAME')
-    elif pool_type=='none': out = x
+  """
+  if pool_type=='avg':
+    out = tf.nn.avg_pool(x, (1, 2, 2, 1), (1, 2, 2, 1), padding='SAME')
+  elif pool_type=='max':
+    out = tf.nn.max_pool(x, (1, 2, 2, 1), (1, 2, 2, 1), padding='SAME')
+  elif pool_type=='none': out = x
 
-    return out
+  return out
 
-  @staticmethod
-  def blur_func(x, kerneldim=15, sigma_sq=1.):
-    """ Convolves spatial dimenstions of x with gaussian rbf kernel
 
-    Args:
-        x (tensor): 4-d (batch x height x width x channels)
-        kerneldim (int>0): preferably odd so kernel centered
-        sigma_sq (float): parameter for rbf
 
-    """
+def calc_2_moments(tensor):
+  """flattens tensor and calculates sample mean and covariance matrix 
+  along last dim (presumably channels)"""
+  
+  shape = tf.shape(tensor, out_type=tf.int32)
+  n = tf.reduce_prod(shape[:-1])
+  
+  flat_array = tf.reshape(tensor, (n, shape[-1]))
+  mu = tf.reduce_mean(flat_array, axis=0, keepdims=True)
+  cov = (tf.matmul(flat_array - mu,flat_array - mu, transpose_a=True)/
+                    tf.cast(n, tf.float32))
+  
+  return mu, cov
 
-    tensor_shape = tf.shape(x)
-    d=tf.range(kerneldim, dtype=tf.float32)
-    d=tf.expand_dims((d-d[kerneldim//2])**2,0)
-    dist=d+tf.transpose(d)
-    kernel=tf.exp(-dist*(1./(2.*sigma_sq)))
-    kernel = kernel/tf.reduce_sum(kernel)
-    kernel = tf.reshape(kernel, (kerneldim, kerneldim,1,1))
-    kernel = tf.tile(kernel, (1,1,tensor_shape[3],1))
 
-    return tf.nn.depthwise_conv2d(x, kernel, [1,1,1,1], padding='SAME')
+
+def calc_l2wass_dist(layer_style_desc, mean_synth, cov_synth):
+  """Calculates (squared) l2-Wasserstein distance between gaussians
+  parameterized by first two moments of style and synth activations"""
+  
+  mean_stl, tr_cov_stl, root_cov_stl = layer_style_desc
+  
+  #tr_cov_synth = tf.trace(cov_synth)
+  tr_cov_synth = tf.reduce_sum(tf.maximum(
+                tf.self_adjoint_eig(cov_synth)[0],0.))
+  
+  
+  mean_diff_squared = tf.reduce_sum(tf.square(mean_stl-mean_synth))
+
+  cov_prod = tf.matmul(tf.matmul(root_cov_stl,cov_synth),root_cov_stl)
+  
+  #trace of sqrt of matrix is sum of sqrts of eigenvalues
+  var_overlap = tf.reduce_sum(tf.sqrt(tf.maximum(
+                tf.self_adjoint_eig(cov_prod)[0],0.1)))
+
+  #loss can be slightly negative because of the 'maximum' on eigvals of cov_prod
+  #could fix with  tr_cov_synth= tf.reduce_sum(tf.maximum(cov_synth,0))
+  #but that would mean extra non-critical computation
+
+  dist = mean_diff_squared+tr_cov_stl+tr_cov_synth-2*var_overlap
+  
+  ### above dist written out in latec:
+  #\mathcal{W}_2(\mathcal{N}(\mu_{x},\Sigma_{x}),\mathcal{N}(\mu_{y},\Sigma_{y}))^2
+  #&= \inf_{g \in G(\mathcal{N}^x,\mathcal{N}^y)} \mathbb{E}_{g}||x-y||^2 \\
+  #&= ||\mu_x-\mu_y||^2 + \mbox{tr} (\Sigma_x)+ \mbox{tr} (\Sigma_y) 
+  #- 2\mbox{tr} \left((\Sigma_y^{\frac{1}{2}}\Sigma_x\Sigma_y^{\frac{1}{2}})^{\frac{1}{2}}\right)
+  
+  
+  return dist
